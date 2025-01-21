d85a72ac094b89508f8938f7acc5d8fb6993bb2a
Joey Stowe <jbstowe@ua.edu>
2025-01-21 12:08:28 -0600

    Update newproject.sh

--- newproject.sh
+++ newproject.sh
@@ -67,6 +67,7 @@ fi
 # fi
 
 # run npm run dev in the bg if theres an app folder and package-lock.json (npm install has been ran)
-if [ -d app ] && [ -d package-lock.json ]; then
+# if [ -d app ] && [ -d package-lock.json ]; then
+if [ -d app ]; then
     docker exec -d app npm run dev
 fi
